96cc8eefa9c43be714f3e572cdb665d98806f9e9
unknown <ljw011381@gmail.com>
2026-07-07 15:57:56 +0900

    init

---
+++ src/test/java/com/zik00/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zik00.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
